af01deda4375536fd1ac24189c2adebab45a9347
Christopher Kim <chris.kim1990@gmail.com>
2021-02-08 18:05:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,6 +13,6 @@ end
 def using_pop(continents)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   deleted_string = "Antarctica"
-  continents.pop(deleted_string)
+  continents.pop
   continents
 end
