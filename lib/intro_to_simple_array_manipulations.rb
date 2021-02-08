7f0d7bbf33733eaed6ffa59de8fa64b2b55188df
Christopher Kim <chris.kim1990@gmail.com>
2021-02-08 18:24:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,6 +18,6 @@ end
 
 def pop_with_args
   @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  @small_dogs = @dog_breeds.pop
+  @small_dogs = @dog_breeds.pop(2)
   @small_dogs
 end
