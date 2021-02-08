d7275ef12d8931a632f237bf6a76dbbd704404a1
Christopher Kim <chris.kim1990@gmail.com>
2021-02-08 18:31:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,3 +27,9 @@ def using_shift(my_favorite_cities)
   @im_so_over_this_city = @my_favorite_cities.shift
   @im_so_over_this_city
 end
+
+def shift_with_args(ice_cream_brands)
+  @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  @brands_removed = @ice_cream_brands.shift(2)
+  @brands_removed
+end
